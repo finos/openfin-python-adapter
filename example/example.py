bff6fa06bfd668be813d9ae62ea5676be7d91bdc
PJ Singh <pj.singh@openfin.co>
2021-05-19 13:06:33 -0400

    added json to python

--- example/example.py
+++ example/example.py
@@ -1,6 +1,7 @@
 import openfin
 import time
 import random
+import json
 
 client = openfin.OpenFinClient()
 
@@ -8,13 +9,15 @@ client = openfin.OpenFinClient()
 def log_message(*args, **kwargs):
     print(args, kwargs)
 
+
 def on_register(val):
     print("Registered")
+
 
 client.subscribe(openfin.SubKey.from_string("example"), log_message, on_register)
 
 for index in range(0, 10):
-    client.publish("example", '{ "name":" ' + str(index) + '"}')
+    client.publish("example", json.loads('{"test":' + str(index) + '}'))
     time.sleep(random.random())
 
 text = input()
