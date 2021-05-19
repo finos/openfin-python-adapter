7a8c55e95b58a20813aa65deeb2c7fad89592de0
PJ Singh <pj.singh@openfin.co>
2021-05-19 13:40:35 -0400

    Update example.py

--- example/example.py
+++ example/example.py
@@ -16,8 +16,23 @@ def on_register(val):
 
 client.subscribe(openfin.SubKey.from_string("example"), log_message, on_register)
 
+payload = {
+  "title": "Salesforce to Purchase Slack for $28Bil",
+  "body": "The two companies have entered into a definitive agreement for sale in cash and stock...\n        ![](https://cdn.openfin.co/examples/notifications/graph1.png)",
+  "icon": "https://cdn.openfin.co/examples/notifications/factset.png",
+  "category": "news",
+  "buttons": [
+    {
+      "title": "Read More",
+      "cta": "true"
+    }
+  ],
+  "catalogId": "c12475ba-9d1b-4ac6-a45f-0e2121902674"
+}
+
 for index in range(0, 10):
-    client.publish("example", json.loads('{"test":' + str(index) + '}'))
+    #client.publish("example", '{ test:' + str(index) + '}')
+    client.publish("example", json.dumps('{'+ str(index) +':' +  str(payload) +'}'))
     time.sleep(random.random())
 
 text = input()
